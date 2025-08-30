8adaadf10442d19f17fb85702bd73be250ca04fd
Sourish Ghosh <sghosh.ile.7@gmail.com>
2025-08-30 10:26:47 +0530

    File Structure updated

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+// Aeris Weather Dashboard - Main JavaScript File
+
+const searchInput = document.querySelector('.search-input');
+const searchBtn = document.querySelector('.search-btn');
+const refreshBtn = document.querySelector('.refresh-btn');
+const settingsBtn = document.querySelector('.settings-btn');
+
+
